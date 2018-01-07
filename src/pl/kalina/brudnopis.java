78f8c00676fae71b63f37fab1effbc670cf3b95f
Kalina5 <kalina51131@hotmail.com>
2018-01-07 19:09:07 +0100

    Mała tablica taka nowa troche mniejsza ale tez nauczy na kolokwium, na tej malej tablicy chce sie nauczyc dodawac i srednia robic

--- src/pl/kalina/brudnopis.java
+++ src/pl/kalina/brudnopis.java
@@ -1,16 +1,25 @@
 package pl.kalina;
 
-//import java.util.Random;
+import java.util.Random;
 
 
 public class brudnopis {
     public static void main(String[] args) {
 
-        int[][] macierz = new int[5][6];
+        Random random = new Random();
+        int c = 5;
+        int d = 5;
+        int[][] macierz = new int[c][d];
         //uzupelnianie macierzy
         for (int a = 0; a < macierz.length; a++) {
             for (int b = 0; b < macierz[a].length; b++) {
-
+                macierz[c][d] = random.nextInt(100);
+            }
+            for (int[] e : macierz) {
+                for (int f : e) {
+                    System.out.println(f);
+                }
+            }
             }
         }
 
